a6d92249dda3e14483a91ed65d3f7f3c89c0fe3c
Flog7877 <flog7877@gmail.com>
2025-01-24 10:24:06 +0100

    Initial commit: Projekt iniitiert und Backend angefangen

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('API läuft!');
+});
+
+const PORT = 3002;
+app.listen(PORT, () => {
+    console.log(`Server läuft! -> http://localhost:${PORT}`);
+});
